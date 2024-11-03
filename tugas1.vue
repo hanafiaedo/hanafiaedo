edd87eb05537f021ddf55be28570c8a1825fed1c
hanafiaedo <146307280+hanafiaedo@users.noreply.github.com>
2024-11-03 11:17:34 +0700

    Create tugas1.vue

---
+++ tugas1.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <h1>Numbers 1 to 50</h1>
+    <ul>
+      <li v-for="number in numbers" :key="number">{{ number }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      numbers: Array.from({ length: 50 }, (v, k) => k + 1)
+    };
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  margin-top: 60px;
+}
+</style>
